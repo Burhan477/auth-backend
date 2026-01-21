77234629adde21dc3985881d5007802500316527
Burhan477 <burhanuddin8866@gmail.com>
2026-01-21 20:16:55 +0530

    update api and package.json

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/items", itemRoutes);
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.send("API is running");
 });
 
